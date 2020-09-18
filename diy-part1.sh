5975a913380ca532a3381a633fdd55cf2abc491e
hhdys412 <307026423@qq.com>
2020-09-18 19:35:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/hhdys412/helloworld.git /workdir/hello
+echo "=========="
 mv /workdir/hello /workdir/openwrt/package/
